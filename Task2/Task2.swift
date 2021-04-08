e64d8097446deb10441849da28e7950593e85e95
Andrey Kindyaev <akindziayeu@scnsoft.com>
2021-04-08 16:48:53 +0300

    Task2

---
+++ Task2/Task2.swift
@@ -0,0 +1,11 @@
+//
+//  Task2.swift
+//
+//  Created by Andrey Kindyaev on 8.04.21.
+//
+
+Input: Array of Strings
+
+Output:
+Maximum length of concatenated string using strings from given Array in which every letter is different or zero
+Array.count in [1..8]
---
+++ Task2/solution.swift
@@ -0,0 +1,42 @@
+//
+//  solution.swift
+//
+//  Created by Andrey Kindyaev on 8.04.21.
+//
+
+import Foundation
+
+public func solution(_ A : inout [String]) -> Int {
+    let strings = A.filter { Set($0).count == $0.count }
+    guard !strings.isEmpty else {
+        return 0
+    }
+    let maxAvailableLength = strings.reduce(0, { $0 + $1.count })
+    // use N bits mask for concatenation combination
+    // N in range [1..8] thus UInt8 is enough
+    let maxBitCombination = UInt8(pow(Double(2), Double(strings.count)))
+    var maxLength = 0
+    main: for bitCombination in 1..<maxBitCombination {
+        var restOfCombination = bitCombination
+        var index = 0
+        var string = ""
+        while restOfCombination != 0 && index < strings.count {
+            let quotientAndRemainder = restOfCombination.quotientAndRemainder(dividingBy: 2)
+            restOfCombination = quotientAndRemainder.quotient
+            if quotientAndRemainder.remainder == 1 {
+                string += strings[index]
+                guard string.count == Set(string).count else {
+                    continue main
+                }
+            }
+            index += 1
+        }
+        if string.count > maxLength {
+            if string.count == maxAvailableLength {
+                return maxAvailableLength
+            }
+            maxLength = string.count
+        }
+    }
+    return maxLength
+}
